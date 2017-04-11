0d475def57893a39887dd3e3fe19f74f775e6cf4
snowgoing <snowgoing@gmail.com>
2017-04-11 14:48:19 -0700

    Fix API Call

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -9,8 +9,8 @@ module.exports = {
     var requestURL = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
     return axios.get(requestURL).then(function(res){
-
-      if (res.data.cod !== 200) {
+      // debugger;
+      if (res.data.message) {
         throw new Error(res.data.message);
       } else {
         return res.data.main.temp;
