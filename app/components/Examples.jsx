33e645775cd916bdb385eedea2f6e3daf2b99438
snowgoing <snowgoing@gmail.com>
2017-04-12 00:43:37 -0700

    Add Centering Styles

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,6 +1,18 @@
 var React = require('react');
 var {Component} = React;
+var {Link} = require('react-router');
 
-var Examples = (props) => <h3>Examples</h3>;
+var Examples = (props) => {
+  return (
+    <div>
+      <h1 className='text-center'>Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li><Link to='/?location=Boston'>Boston</Link></li>
+        <li><Link to='/?location=Lisbon'>Lisbon</Link></li>
+      </ol>
+    </div>
+  )
+};
 
 module.exports = Examples;
