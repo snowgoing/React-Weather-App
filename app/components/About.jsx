3b73d35d1cfca84cbc8f04f1fe5ddf10a90b7923
snowgoing <snowgoing@gmail.com>
2017-04-12 10:56:01 -0700

    Style about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,21 @@
 var React = require('react');
 
-var About = (props) => <h2>About Refactored Component</h2>;
+var About = (props) => {
+  return (
+    <div>
+      <h1 className='text-center'>About</h1>
+      <p>This is a weather application built on React.</p>
+      <p>Here are some of the tools used</p>
+      <ol>
+        <li>
+          <a href='https://facebook.github.io/react'>React</a> - Javascript framework used
+        </li>
+        <li>
+          <a href='http://openweathermap.org'>Open Weather Map</a> - Searches by city name
+        </li>
+      </ol>
+    </div>
+  )
+};
 
 module.exports = About;
